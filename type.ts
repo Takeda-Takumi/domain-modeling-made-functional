1460e7f3efd271241b874c7fc13d62b121204d66
local <91676218+Takeda-Takumi@users.noreply.github.com>
2024-07-06 13:38:18 +0900

    5.3

---
+++ type.ts
@@ -0,0 +1,9 @@
+import { number } from "fp-ts"
+
+type decimal = number
+type int = number
+
+type CustomerId = int
+type WidgetCode = string
+type UnitQuantity = int
+type KilogramQuatity = decimal
